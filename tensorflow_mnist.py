5226588041018dca396e61e53ce0d864c979257f
Jack Zhou <bumble.zhou@gmail.com>
2019-12-02 14:08:27 +0800

    add mnist dataset to local folder

--- tensorflow_mnist.py
+++ tensorflow_mnist.py
@@ -43,7 +43,9 @@ def imageprepare(argv):
 
 def main():
     mnist = tf.keras.datasets.mnist
-    (x_train, y_train), (x_test, y_test) = mnist.load_data(path='./data/mnist.pkl.gz')
+    # On windows, place mnist.npz to folder: C:\Users\<UserName>\.keras\datasets
+    # On Linux, place mnist.npz to folder: ~/.keras/datasets/
+    (x_train, y_train), (x_test, y_test) = mnist.load_data(path='mnist.npz')
     x_train, x_test = x_train / 1024, x_test / 1024
     model = tf.keras.models.Sequential([
         tf.keras.layers.Flatten(input_shape=(28, 28)),
@@ -54,7 +56,7 @@ def main():
 
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-    model.fit(x_train, y_train, epochs=5)
+    model.fit(x_train, y_train, epochs=10)
     model.evaluate(x_test, y_test, verbose=2)
 
     # imvalue = imageprepare(sys.argv)
