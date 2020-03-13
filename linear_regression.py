c79bd7a815377e644e9b3bbe0dc36dd1aaf97d12
bumble.zhou <bumble.zhou@gmail.com>
2020-03-13 11:51:16 +0800

    only executing necessary loops

--- linear_regression.py
+++ linear_regression.py
@@ -9,7 +9,7 @@ def simple_linear_regression():
     # create a dataset for input(x) and output(y)
     y = []
     x = []
-    for i in range(-100, 100):
+    for i in range(0, 2):
         x.append(list([i]))
         y.append(5.5 + 4.7 * i)
 
@@ -31,7 +31,7 @@ def simple_linear_regression():
     print('predicted response:', y_pred, sep='\n')
 
     # Predict new data
-    x_new = [list([item]) for item in range(101, 151)]
+    x_new = [list([item]) for item in range(10, 16)]
     print('x_new:', x_new, sep='\n')
     y_new = model.predict(x_new)
     print('predicted new:', y_new, sep='\n')
@@ -43,7 +43,7 @@ def multiple_linear_regression():
     # create a dataset for input(a, m, n) and output(y)
     y = []
     x = []
-    for i in range(-100, 100):
+    for i in range(0, 6):
         a = random.randint(0, 100)
         m = random.randint(0, 100)
         n = random.randint(0, 100)
@@ -68,7 +68,7 @@ def multiple_linear_regression():
     print('predicted response:', y_pred, sep='\n')
 
     # Predict new data
-    x_new = [list([item, item, item]) for item in range(101, 151)]
+    x_new = [list([random.randint(0, 100), random.randint(0, 100), random.randint(0, 100)]) for item in range(10, 16)]
     print('x_new:', x_new, sep='\n')
     y_new = model.predict(x_new)
     print('predicted new:', y_new, sep='\n')
@@ -110,8 +110,7 @@ def ploynomial_regression():
     print('intercept:', model.intercept_)
     print('equation: y = 3 - (5 * a) + (6 * b) + 4 * (a * a) - 7 * a * b + 2 * (b * b)')
     print("expected slope:[3, -5, 6, 4, -7, 2]")
-    print('slope:')
-    print(', '.join('{:.2f}'.format(f) for f in model.coef_))
+    print('slope:', model.coef_)
 
     # Predict response
     print('predicted [[28, 47]]:')
